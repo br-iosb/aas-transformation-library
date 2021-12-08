164ac6a2c755c0bb1ad69941d937b6baafeb04cc
br-iosb <70897399+br-iosb@users.noreply.github.com>
2021-12-08 18:25:37 +0100

    demo scenarios

--- src/main/java/demo/DemoRunner.java
+++ src/main/java/demo/DemoRunner.java
@@ -1,5 +1,6 @@
 package demo;
 
+import java.io.Console;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -26,12 +27,17 @@ public class DemoRunner {
 	private static final String OUT = "output.json";
 
 	private static final String _01_FULL_SUPPORT = "01_fullSupport";
+	private static final String _02_EXPRESSIONS = "02_bindWithExpression";
+	private static final String _03_VARS = "03_vars";
+	private static final String _04_FOREACH = "04_foreach";
+	private static final String _05_DEFINITIONS = "05_definitions";
+	private static final String _06_VARSCOPE = "06_variableScope";
 
 	public static void main(String[] args) throws Exception {
-
+		String scenarioName = _06_VARSCOPE;
+		
 		Path demoSpace = Paths.get("src/main/resources/demo").toAbsolutePath();
-
-		Path execDir = demoSpace.resolve(_01_FULL_SUPPORT);
+		Path execDir = demoSpace.resolve(scenarioName);
 
 		Path pathIn = execDir.resolve(IN);
 		Path pathConfig = execDir.resolve(CONFIG);
@@ -46,6 +52,8 @@ public class DemoRunner {
 			try (OutputStream outputStream = Files.newOutputStream(pathOut, StandardOpenOption.CREATE,
 					StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING)) {
 				new JsonSerializer().write(outputStream, transformationResults);
+				System.out.println(scenarioName);
+				new JsonSerializer().write(System.out, transformationResults);
 			}
 		}
 
